f8a54db7fa17a0019c71f56ae6aebe5b84307192
julie0919 <julie.lee0919@gmail.com>
2021-01-13 13:34:24 +0900

    v02

---
+++ app/src/main/java/com/eomcs/pms/App3.java
@@ -0,0 +1,9 @@
+package com.eomcs.pms;
+
+public class App3 {
+  public static void main(String[] args) { 
+    System.out.println("[작업]\n프로젝트: 미니 프로젝트 관리 시스템 개발");
+    System.out.println("번호: 1\n내용: 요구사항 수집\n완료일: 2020-01-20");
+    System.out.println("상태: 진행중\n담당자: 홍길동");
+  }
+}

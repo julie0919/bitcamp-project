5f898d2621c987e9f4b960f98580ddc49fcf8fc7
julie0919 <julie.lee0919@gmail.com>
2021-01-21 16:56:28 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,5 +1,9 @@
 package com.eomcs.pms;
 
+import com.eomcs.pms.handler.BoardHandler;
+import com.eomcs.pms.handler.BoardHandler2;
+import com.eomcs.pms.handler.BoardHandler3;
+import com.eomcs.pms.handler.BoardHandler4;
 import com.eomcs.pms.handler.MemberHandler;
 import com.eomcs.pms.handler.ProjectHandler;
 import com.eomcs.pms.handler.TaskHandler;
@@ -42,6 +46,31 @@ public class App {
           case "/task/list":
             TaskHandler.list();
             break;
+          case "/board/add":
+            BoardHandler.add();
+            break;
+          case "/board/list":
+            BoardHandler.list();
+            break;
+          case "/board2/add":
+            BoardHandler2.add();
+            break;
+          case "/board2/list":
+            BoardHandler2.list();
+            break;
+          case "/board3/add":
+            BoardHandler3.add();
+            break;
+          case "/board3/list":
+            BoardHandler3.list();
+            break;
+          case "/board4/add":
+            BoardHandler4.add();
+            break;
+          case "/board4/list":
+            BoardHandler4.list();
+            break;
+
           case "quit":
           case "exit":
             System.out.println("안녕!");
